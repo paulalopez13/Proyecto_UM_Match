8e3a2d6be8e2617b62dbf1f071a074bc4b17b5e9
Eloisa Lema <lemaeloisa@gmail.com>
2026-06-14 08:53:20 -0300

    comentarios

--- utilidades.pl
+++ utilidades.pl
@@ -1,7 +1,4 @@
-% - - - - - - - - - - - - - - - - - - - - - - - -
-%  - - - - - - - - - - - - - - - - - - - - - - - -
-
-  % QUITAR_ELEMENTO - Sacar un elemento en específico
+% QUITAR_ELEMENTO - Sacar un elemento en específico
 
 % caso base: lista vacía
 quitar_elemento(_, [], []).    % _ para avisarle a prolog que esa variable no la voy a usar a propósito
@@ -13,11 +10,7 @@ quitar_elemento(Elemento, [Elemento|T], T) :- !.
 quitar_elemento(Elemento, [H|T], [H|Resultado]) :-
   quitar_elemento(Elemento, T, Resultado).
 
-
-% - - - - - - - - - - - - - - - - - - - - - - - -
-%  - - - - - - - - - - - - - - - - - - - - - - - -
-
-  % TOP_N - agarrar los primeros N elementos de una lista
+% TOP_N - agarrar los primeros N elementos de una lista
 
 % caso base: agarré los que necesitaba
 top_n(_, 0, []) :- !.
@@ -31,21 +24,13 @@ top_n([H|T], N, [H|Restante]) :-
   N1 is N - 1,
   top_n(T, N1, Restante).
 
-
-% - - - - - - - - - - - - - - - - - - - - - - - -
-%  - - - - - - - - - - - - - - - - - - - - - - - -
-
-%   EN_RANGO - muestra si un valor pertenece a un rango dado
+% EN_RANGO - muestra si un valor pertenece a un rango dado
 
 en_rango(Valor, Min, Max) :- 
   Valor >= Min,
   Valor =< Max.
 
-
-% - - - - - - - - - - - - - - - - - - - - - - - -
-%  - - - - - - - - - - - - - - - - - - - - - - - -
-
-  % MOSTRAR_LISTA_CON_SEPARADOR - imprime lista separada por caracter dado
+% MOSTRAR_LISTA_CON_SEPARADOR - imprime lista separada por caracter dado
 
 % caso base: lista vacía
 mostrar_lista_con_separador([], _).
@@ -59,11 +44,8 @@ mostrar_lista_con_separador([H|T], Separador) :-
   write(H), write(Separador),
   mostrar_lista_con_separador(T, Separador).
 
+% CAPITALIZAR - convierte la primera letra a mayúscula
 
-% - - - - - - - - - - - - - - - - - - - - - - - -
-%  - - - - - - - - - - - - - - - - - - - - - - - -
-
-  % CAPITALIZAR - convierte la primera letra a mayúscula
 capitalizar(Palabra, Capitalizado) :-
   atom_chars(Palabra, [Primera|Resto]),              % separo primera letra del resto
   upcase_atom(Primera, PrimeraMayus),                % paso primera letra a mayúscula
